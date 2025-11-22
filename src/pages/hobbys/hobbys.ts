771276f0b1812fcc7a85fe6597628141dce4f0aa
sebastbas2310 <sbermudezburitica@email.com>
2025-11-22 18:57:14 -0500

    inicio portafolio

---
+++ src/pages/hobbys/hobbys.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hobbys',
+  imports: [],
+  templateUrl: './hobbys.html',
+  styleUrl: './hobbys.scss',
+})
+export class Hobbys {
+
+}
